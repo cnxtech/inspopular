61c79a8805c26cb1b0b5a69f235baba619a48484
danielkvist <d94.zaragoza@gmail.com>
2018-10-06 19:58:47 +0200

    refactor: hashtag package

--- pkg/hashtag/hashtag.go
+++ pkg/hashtag/hashtag.go
@@ -5,14 +5,9 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"regexp"
 	"strconv"
-	"sync"
-	"text/tabwriter"
 )
-
-type List []*hashtag
 
 type hashtag struct {
 	tag   string
@@ -21,41 +16,6 @@ type hashtag struct {
 }
 
 const instaURL = "https://www.instagram.com/explore/tags/"
-
-func CreateList(tags []string) *List {
-	var w sync.WaitGroup
-	var list List
-
-	for _, t := range tags {
-		list = append(list, newhashtag(t))
-	}
-
-	w.Add(len(tags))
-	for i := range list {
-		go func(i int) {
-			defer w.Done()
-			if err := list[i].popularity(); err != nil {
-				log.Println(err)
-			}
-		}(i)
-	}
-
-	w.Wait()
-	return &list
-}
-
-func (l *List) Print() {
-	const format = "%v\t%v\t%v\t\n"
-	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
-	fmt.Fprintf(tw, format, "Hashtag", "URL", "Posts")
-	fmt.Fprintf(tw, format, "-------", "---", "-----")
-
-	fmt.Println()
-	for _, item := range *l {
-		fmt.Fprintf(tw, format, item.tag, item.url, item.posts)
-	}
-	tw.Flush()
-}
 
 func newhashtag(tag string) *hashtag {
 	h := &hashtag{tag: tag, url: instaURL + tag}
---
+++ pkg/hashtag/list.go
@@ -0,0 +1,46 @@
+package hashtag
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"sync"
+	"text/tabwriter"
+)
+
+type List []*hashtag
+
+func CreateList(tags []string) *List {
+	var w sync.WaitGroup
+	var list List
+
+	for _, t := range tags {
+		list = append(list, newhashtag(t))
+	}
+
+	w.Add(len(tags))
+	for i := range list {
+		go func(i int) {
+			defer w.Done()
+			if err := list[i].popularity(); err != nil {
+				log.Println(err)
+			}
+		}(i)
+	}
+
+	w.Wait()
+	return &list
+}
+
+func (l *List) Print() {
+	const format = "%v\t%v\t%v\t\n"
+	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 4, 4, ' ', 0)
+	fmt.Fprintf(tw, format, "Hashtag", "URL", "Posts")
+	fmt.Fprintf(tw, format, "-------", "---", "-----")
+
+	fmt.Println()
+	for _, item := range *l {
+		fmt.Fprintf(tw, format, item.tag, item.url, item.posts)
+	}
+	tw.Flush()
+}
