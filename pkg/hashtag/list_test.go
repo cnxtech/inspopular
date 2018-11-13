dd83af83de0a065c8d306a90c6195c0be59ecbde
danielkvist <d94.zaragoza@gmail.com>
2018-11-13 22:55:11 +0100

    improve: legibility of the tests
    Improve the legibility of the tests and the performance of the helper function.

--- pkg/hashtag/list_test.go
+++ pkg/hashtag/list_test.go
@@ -19,51 +19,34 @@ func TestCreateList(t *testing.T) {
 		{
 			tag:   "golang",
 			url:   ts.URL + "/golang",
-			posts: 420,
-		},
-		{
-			tag:   "docker",
-			url:   ts.URL + "/docker",
-			posts: 420,
-		},
-		{
-			tag:   "pet",
-			url:   ts.URL + "/pet",
-			posts: 420,
+			posts: 1200,
 		},
 		{
 			tag:   "error",
 			url:   ts.URL + "/error",
 			posts: 0,
 		},
-		{
-			tag:   "cat",
-			url:   ts.URL + "/cat",
-			posts: 420,
-		},
 	}
 
 	hashtags := make([]string, len(tests))
-
 	for i := range tests {
 		hashtags[i] = tests[i].tag
 	}
 
 	l := CreateList(hashtags, ts.URL+"/")
-
 	for i, tt := range tests {
 		listItem := (*l)[i]
 
 		if tt.tag != listItem.tag {
-			t.Fatalf("expected tag %q posts. got=%q", tt.tag, listItem.tag)
+			t.Errorf("expected tag %q posts. got=%q", tt.tag, listItem.tag)
 		}
 
 		if tt.url != listItem.url {
-			t.Fatalf("in tag %q expected %s as URL. got=%s", tt.tag, tt.url, listItem.url)
+			t.Errorf("in tag %q expected %s as URL. got=%s", tt.tag, tt.url, listItem.url)
 		}
 
 		if tt.posts != listItem.posts {
-			t.Fatalf("in tag %q expected %v posts. got=%v", tt.tag, tt.posts, listItem.posts)
+			t.Errorf("in tag %q expected %v posts. got=%v", tt.tag, tt.posts, listItem.posts)
 		}
 	}
 }
@@ -72,19 +55,21 @@ func BenchmarkCreatrList(b *testing.B) {
 	ts := httptest.NewServer(http.HandlerFunc(testHandler))
 	defer ts.Close()
 
+	url := ts.URL + "/"
 	hashtags := []string{"go", "golang", "docker", "cat", "pet"}
-
 	for i := 0; i < b.N; i++ {
-		CreateList(hashtags, ts.URL+"/")
+		CreateList(hashtags, url)
 	}
 }
 
 func testHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+
 	switch r.URL.Path {
 	case "/error":
 		w.WriteHeader(http.StatusNotFound)
 	default:
 		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, "count: %d", 420)
+		fmt.Fprintf(w, "<h1>Simulated counter</h1><p>\"count\": %d<p><p>count: 250</p><p>counting : 500</p>}", 1200)
 	}
 }
